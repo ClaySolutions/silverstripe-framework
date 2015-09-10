11aaa6a8be0effac7bf9d98f2091b748b69c84fa
Lennart Pol <lennart@my-clay.com>
2015-09-10 14:19:27 +0200

    fix ss bug

--- model/HasManyList.php
+++ model/HasManyList.php
@@ -9,13 +9,13 @@
 class HasManyList extends RelationList {
 
 	protected $foreignKey;
-	
+
 	/**
 	 * Create a new HasManyList object.
 	 * Generation of the appropriate record set is left up to the caller, using the normal
 	 * {@link DataList} methods.  Addition arguments are used to support {@@link add()}
 	 * and {@link remove()} methods.
-	 * 
+	 *
 	 * @param string $dataClass The class of the DataObjects that this will list.
 	 * @param string $foreignKey The name of the foreign key field to set the ID filter against.
 	 */
@@ -39,10 +39,10 @@ class HasManyList extends RelationList {
 
 		// Apply relation filter
 		if(is_array($id)) {
-			return "\"$this->foreignKey\" IN ('" .
+			return "\"$this->dataClass\".\"$this->foreignKey\" IN ('" .
 				implode("', '", array_map('Convert::raw2sql', $id)) . "')";
 		} else if($id !== null){
-			return "\"$this->foreignKey\" = '" . 
+			return "\"$this->dataClass\".\"$this->foreignKey\" = '" .
 				Convert::raw2sql($id) . "'";
 		}
 	}
@@ -52,7 +52,7 @@ class HasManyList extends RelationList {
 	 *
 	 * It does so by setting the relationFilters.
 	 *
-	 * @param $item The DataObject to be added, or its ID 
+	 * @param $item The DataObject to be added, or its ID
 	 */
 	public function add($item) {
 		if(is_numeric($item)) {
@@ -91,13 +91,13 @@ class HasManyList extends RelationList {
 
 		return $this->remove($item);
 	}
-	
+
 	/**
 	 * Remove an item from this relation.
 	 * Doesn't actually remove the item, it just clears the foreign key value.
-	 * 
+	 *
 	 * @param $item The DataObject to be removed
-	 * @todo Maybe we should delete the object instead? 
+	 * @todo Maybe we should delete the object instead?
 	 */
 	public function remove($item) {
 		if(!($item instanceof $this->dataClass)) {
