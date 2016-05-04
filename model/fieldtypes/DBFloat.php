e9571d10896a30f760399cd9b269b616d25b7927
Lennart Pol <lennart@my-clay.com>
2016-05-04 13:19:40 +0200

    change to support php7

--- model/fieldtypes/DBFloat.php
+++ model/fieldtypes/DBFloat.php
@@ -1,38 +1,38 @@
 <?php
 /**
  * Represents a floating point field.
- * 
+ *
  * @package framework
  * @subpackage model
  */
-class Float extends DBField {
+class DBFloat extends DBField {
 
 	public function __construct($name = null, $defaultVal = 0) {
 		$this->defaultVal = is_float($defaultVal) ? $defaultVal : (float) 0;
-		
+
 		parent::__construct($name);
 	}
-	
+
 	public function requireField() {
 		$parts=Array(
 			'datatype'=>'float',
 			'null'=>'not null',
 			'default'=>$this->defaultVal,
 			'arrayValue'=>$this->arrayValue);
-		
+
 		$values=Array('type'=>'float', 'parts'=>$parts);
 		DB::requireField($this->tableName, $this->name, $values);
 	}
-	
+
 	/**
 	 * Returns the number, with commas and decimal places as appropriate, eg “1,000.00”.
-	 * 
+	 *
 	 * @uses number_format()
 	 */
 	public function Nice() {
 		return number_format($this->value, 2);
 	}
-	
+
 	public function Round($precision = 3) {
 		return round($this->value, $precision);
 	}
@@ -40,7 +40,7 @@ class Float extends DBField {
 	public function NiceRound($precision = 3) {
 		return number_format(round($this->value, $precision), $precision);
 	}
-	
+
 	public function scaffoldFormField($title = null) {
 		return new NumericField($this->name, $title);
 	}
